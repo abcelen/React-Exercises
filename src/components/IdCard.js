047470e59accf88fac4230040931f98b946390d1
abcelen <abdullahcelen009@gmail.com>
2021-01-01 22:08:08 +0000

    tweaks

--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -3,13 +3,17 @@ import React from 'react'
 export default function IdCard(props) {
     const {lastname, firstName, gender, birth, picture} = props
     return (
-    
-        <div className = "userid">
-          <p>Last Name: {lastname} </p> 
+        <div className="container">
+    <div className = "row userid">
+    <div className ="col-3">          <img src= {picture} alt="picture" class="img-thumbnail"/>  </div>
+        <div className = "col-3">
+          <p><b>Last Name: </b> {lastname} </p> 
           <p>Name: {firstName} </p> 
           <p>Gender: {gender} </p> 
           <p>birth: {birth} </p> 
-          <p>picture: {picture} </p> 
+
+        </div>
+        </div>
         </div>
     )
 }
